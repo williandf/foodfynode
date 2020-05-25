385cde43f653e550808a691403dad03ac42d1d79
Willian Del Freu <williankeyex@gmail.com>
2020-05-25 17:41:28 -0300

    Refatorando Foodfy

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const nunjuncks = require('nunjucks');
+
+const server = express();
+const recipes = require('./data');
+
+server.use(express.static('public'));
+
+server.set("view engine", "njk");
+
+nunjuncks.configure("views", {
+  express: server,
+});
+
+
+server.get("/", function (req, res) {
+  return res.render("initial");
+});
+
+server.get("/about", function (req, res) {
+  return res.render("about");
+});
+
+server.get("/recipes", function (req, res) {
+  return res.render("recipes", {items: recipes});
+});
+
+server.listen(5000, function() {
+  console.log("Server is running")
+});
